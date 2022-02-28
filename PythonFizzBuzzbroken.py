cde868a9d544850cdf2156a279ec3d71b9fe9674
BryceGreenleaf <70154552+BryceGreenleaf@users.noreply.github.com>
2022-02-28 15:19:33 -0700

    Update PythonFizzBuzzbroken.py

--- PythonFizzBuzzbroken.py
+++ PythonFizzBuzzbroken.py
@@ -1,17 +1,17 @@
-Number == 0
+Number = 0 #Took away the == and changed it to =
 while Number != 100:
- Number == Number + 1
- Buzz == int(Number % 5  == 0)
- Fizz == int(Number % 3 == 0)
- FizzBuzz == int(Number % 15 == 0)
+ Number = Number + 1
+ Buzz = int(Number % 5  == 0) #Took away the == and changed it to =
+ Fizz = int(Number % 3 == 0) #Took away the == and changed it to =
+ FizzBuzz = int(Number % 15 == 0) #Took away the == and changed it to =
 
- if FizzBuzz = True:
-  print(Fizzbuzz)
+ if FizzBuzz == True:
+  print('Fizzbuzz')#added quotes to get the print statements
  else:
-  if Fizz = True:
-    print(Fizz)
+  if Fizz == True:
+    print('Fizz')#added quotes to get the print statements
   else:
-    if Buzz = True:
-        print(Buzz)
+    if Buzz == True:
+        print('Buzz')#added quotes to get the print statements
     else:
-     print(Number)
+     print('Number') #added quotes to get the print statements
