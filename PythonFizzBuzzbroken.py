411ded2a632fcddaebe70d6ee442baa6fa2d8def
ConorOflynn <100319126+ConorOflynn@users.noreply.github.com>
2022-02-28 14:24:12 -0700

    Add files via upload

---
+++ PythonFizzBuzzbroken.py
@@ -0,0 +1,17 @@
+Number == 0
+while Number != 100:
+ Number == Number + 1
+ Buzz == int(Number % 5  == 0)
+ Fizz == int(Number % 3 == 0)
+ FizzBuzz == int(Number % 15 == 0)
+
+ if FizzBuzz = True:
+  print(Fizzbuzz)
+ else:
+  if Fizz = True:
+    print(Fizz)
+  else:
+    if Buzz = True:
+        print(Buzz)
+    else:
+     print(Number)
